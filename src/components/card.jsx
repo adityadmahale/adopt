ca781d8e9b82d71afe2f0ef4c988feb5c7d88e3a
Aditya Mahale <aditya.mahale@enterprisedb.com>
2020-08-01 13:14:35 +0530

    Include add button in the card

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -4,9 +4,16 @@ const Card = ({ title, description, imagePath }) => {
   return (
     <div className="card mt-3">
       <img className="card-img-top" src={imagePath} alt="Houseplant" />
-      <div className="card-body">
+      <div className="card-body pt-1">
         <h5 className="card-title">{title}</h5>
-        <p className="card-text">{description}</p>
+        <div className="row">
+          <div className="col-6 align-self-center">
+            <p className="card-text">Details</p>
+          </div>
+          <div className="col-6 d-flex justify-content-center">
+            <button className="btn item-selected px-4">Add</button>
+          </div>
+        </div>
       </div>
     </div>
   );
