4b47bf5b89b6000506336decc814d28798ced3ac
Aditya Mahale <aditya.mahale@enterprisedb.com>
2020-08-02 13:35:53 +0530

    Add register content

--- src/components/register.jsx
+++ src/components/register.jsx
@@ -1,7 +1,49 @@
 import React from "react";
+import Form from "./commons/form";
+import Joi from "joi";
 
-const Register = () => {
-  return <h1>Register</h1>;
-};
+class Register extends Form {
+  state = {
+    body: {
+      username: "",
+      email: "",
+      password: "",
+    },
+    error: {
+      username: "",
+      email: "",
+      password: "",
+    },
+  };
+
+  handleSubmit = () => {
+    console.log("Submitted");
+  };
+
+  schema = Joi.object({
+    username: Joi.string().min(3).max(50).required(),
+    email: Joi.string()
+      .min(5)
+      .max(50)
+      .email({ tlds: { allow: false } })
+      .required(),
+    password: Joi.string().min(5).max(50).required(),
+  });
+
+  render() {
+    return (
+      <div className="card w-75 mx-auto">
+        <div className="card-body">
+          <form>
+            {this.renderInput("text", "username", "Username")}
+            {this.renderInput("text", "email", "Email")}
+            {this.renderInput("password", "password", "Password")}
+            {this.renderButton("Register")}
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Register;
