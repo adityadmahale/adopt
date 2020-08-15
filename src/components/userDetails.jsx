607bbc4180279ab8147c4665c10b9b9a18905f2c
Aditya Mahale <aditya.mahale@enterprisedb.com>
2020-08-15 17:26:38 +0530

    Implement pagination for history table

--- src/components/userDetails.jsx
+++ src/components/userDetails.jsx
@@ -1,9 +1,17 @@
 import React, { Component } from "react";
 import { getCarts } from "../services/cartService";
+import Pagination from "./pagination";
+import { paginate } from "../utils/paginate";
 
 class UserDetails extends Component {
   state = {
     carts: [],
+    pageSize: 6,
+    pageNumber: 1,
+  };
+
+  handlePageChange = (pageNumber) => {
+    this.setState({ pageNumber });
   };
 
   componentDidMount = async () => {
@@ -11,39 +19,54 @@ class UserDetails extends Component {
     this.setState({ carts });
   };
 
-  renderTable = () => {
+  renderTable = (carts, cartsSize) => {
+    if (cartsSize < 1) return <p>No adoption history</p>;
+
     return (
-      <table className="table">
-        <thead>
-          <tr className="row">
-            <th className="col-8">Plants</th>
-            <th className="col-4">Date</th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.state.carts.map((cart) => {
-            const d = new Date(cart.dateAdopted);
-            return (
-              <tr className="row" key={cart._id}>
-                <td className="col-8">{cart.plants.join(", ")}</td>
-                <td className="col-4">{`${d.getDate()}/${
-                  d.getMonth() + 1
-                }/${d.getFullYear()}`}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <React.Fragment>
+        <h5>Adoption history</h5>
+        <table className="table">
+          <thead>
+            <tr className="row">
+              <th className="col-8">Plants</th>
+              <th className="col-4">Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            {carts.map((cart) => {
+              const d = new Date(cart.dateAdopted);
+              return (
+                <tr className="row" key={cart._id}>
+                  <td className="col-8">{cart.plants.join(", ")}</td>
+                  <td className="col-4">{`${d.getDate()}/${
+                    d.getMonth() + 1
+                  }/${d.getFullYear()}`}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </React.Fragment>
     );
   };
 
   render() {
+    const { carts: allCarts, pageSize, pageNumber } = this.state;
     const { user } = this.props;
+    const cartsSize = allCarts.length;
+
+    const carts = paginate(allCarts, pageSize, pageNumber);
+
     return user ? (
       <div className="mx-auto card p-3 cart">
         <h1>{user.username}</h1>
-        <h5>Adoption history</h5>
-        {this.renderTable()}
+        {this.renderTable(carts, cartsSize)}
+        <Pagination
+          pageSize={pageSize}
+          itemNumbers={cartsSize}
+          pageNumber={pageNumber}
+          onPageChange={this.handlePageChange}
+        />
       </div>
     ) : null;
   }
