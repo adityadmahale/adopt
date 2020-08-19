2bec6d41ad42b1520d82f427cff6307e524b65bb
Aditya Mahale <aditya.mahale@enterprisedb.com>
2020-08-19 14:03:25 +0530

    Update home page content

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -13,20 +13,18 @@ const Home = () => {
         />
         <div className="card-body text-justify">
           <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum. but also the leap into
-            electronic typesetting, remaining essentially unchanged. It was
-            popularised in the 1960s with the release of Letraset sheets
-            containing Lorem Ipsum passages, and more recently with desktop
-            publishing software like Aldus PageMaker including versions of Lorem
-            Ipsum.
+            People lined their windowsills with greenery in increasing numbers
+            after NASA released a series of studies dating back to the late
+            ’80s. Its results suggested that, in addition to absorbing carbon
+            dioxide and releasing oxygen through photosynthesis, certain common
+            indoor plants may also provide a natural way of removing volatile
+            organic pollutants (benzene, formaldehyde, and trichloroethylene
+            were tested). Indoor plants have air-boosting and health benefits
+            that you don’t have to create a wall-to-wall jungle to enjoy. Even a
+            modest amount of foliage might enhance indoor air quality.
+            Unfortunately, office plants are often discarded and abandoned in
+            the alleys. This platform provides you an opportunity to adopt these
+            indoor plants.
           </p>
           <Link className="btn item-selected px-4" to="/shop">
             Proceed
